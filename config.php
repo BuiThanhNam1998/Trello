8d764315a232c9cc3623418a679e6a44f6127934
= <=>
2019-10-26 16:19:30 +0700

    add config

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+	// database
+	define('HOST',         'localhost');
+    define('DB_NAME',      'trello');
+    define('DB_USER',      'root');
+    define('DB_PASSWORD',  '');
+    //domain
+    define('DOMAIN',       'trello');
+	define('WEB_DOMAIN',   'http://localhost/'.DOMAIN);
+	// ASSET
+	define('DIR_THEMES',   'assets');
+	define('URL_IMAGES', 	WEB_DOMAIN.'/'.DIR_THEMES.'/images');
+	define('URL_CSS', 		WEB_DOMAIN.'/'.DIR_THEMES.'/css');
+	define('URL_JS', 		WEB_DOMAIN.'/'.DIR_THEMES.'/js');
+?>
